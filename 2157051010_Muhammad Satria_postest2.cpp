a5a83a33e67ed814157923977e891230e3ab349f
MuhammadSatria <92299364+MuhammadSatria09@users.noreply.github.com>
2021-12-16 16:07:48 +0700

    Add files via upload

---
+++ 2157051010_Muhammad Satria_postest2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+	cout<<"Post Test DDP 2\nMuhammad Satria\n2157051010\n";
+	cout<<"menghitung nilai dari fungsi ax^2 + bx + c dengan nilai a, b, c, dan x berasal dari input\n";
+	cout<<"--------------------------------------------------------------------------------------------"<<endl;
+	
+	float a;
+	float b;
+	float c;
+	float x;
+	
+	//Input
+	cout<<"a=";cin>>a;
+	cout<<"b=";cin>>b;
+	cout<<"c=";cin>>c;
+	cout<<"x=";cin>>x;
+	
+	//ax^2 + bx + c
+	
+	//Output
+	cout<<"--------------------------------------------------------------------------------------------"<<endl;
+	cout<<"ax^2 + bx + c="<<a*(x*x)+b*x+c;
+		
+}
